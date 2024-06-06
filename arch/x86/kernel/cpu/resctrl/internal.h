kernelorg-mirror/linux_kernel_git_morse_linux

x86/resctrl: Make resctrl_arch_rmid_read() retry when it is interrupted
resctrl_arch_rmid_read() could be called by resctrl in process context,
and then called by the PMU driver from irq context on the same CPU.
This could cause struct arch_mbm_state's prev_msr value to go backwards,
leading to the chunks value being incremented multiple times.

The struct arch_mbm_state holds both the previous msr value, and a count
of the number of chunks. These two fields need to be updated atomically.

Read the prev_msr before accessing the hardware, and cmpxchg() the value
back. If the value has changed, the whole thing is re-attempted.

Signed-off-by: James Morse <james.morse@arm.com>

arch/x86/kernel/cpu/resctrl/internal.h
@@ -8,6 +8,7 @@
 #include <linux/fs_context.h>
 #include <linux/jump_label.h>
 #include <linux/tick.h>
+#include <asm/atomic.h>
 #include <asm/resctrl.h>
 
 #define L3_QOS_CDP_ENABLE		0x01ULL
@@ -338,8 +339,8 @@ struct mbm_state {
  *		find this struct.
  */
 struct arch_mbm_state {
-	u64	chunks;
-	u64	prev_msr;
+	atomic64_t	chunks;
+	atomic64_t	prev_msr;
 };
 
 /**

KobaKoNvidia/linux-nvidia-6.5

Similarly __rmid_read() must write to one MSR and read from another,
this must be proteted from re-entrance.
back. If the value has changed, the whole thing is re-attempted. To protect
the MSR, __rmid_read() will retry reads for QM_CTR if QM_EVTSEL has changed
from the selected value.
---
Changes since v4:
 * Added retry loop in __rmid_read() to protect the CPU MSRs.
Signed-off-by: Tushar Dave <tdave@nvidia.com>
(cherry picked from commit caad81b4619454d4a89b6c4aad8394e18aec8228)
Signed-off-by: kobak <kobak@nvidia.com>

arch/x86/kernel/cpu/resctrl/internal.h
@@ -2,6 +2,7 @@
 #ifndef _ASM_X86_RESCTRL_INTERNAL_H
 #define _ASM_X86_RESCTRL_INTERNAL_H
 
+#include <linux/atomic.h>
 #include <linux/resctrl.h>
 #include <linux/sched.h>
 #include <linux/kernfs.h>
@@ -338,8 +339,8 @@ struct mbm_state {
  *		find this struct.
  */
 struct arch_mbm_state {
-	u64	chunks;
-	u64	prev_msr;
+	atomic64_t	chunks;
+	atomic64_t	prev_msr;
 };
 
 /**